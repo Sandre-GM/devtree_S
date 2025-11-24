1d0afdc75db6cfe797fe2fad4cdad29223ebedfc
Mauricio Gonzalez Marmolejo <sandre9477@gmail.com>
2025-11-24 15:29:46 -0600

    Hash of user passwords with new auth.ts file

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(409).json({ error: error.message });
+  }
+
   const user = new User(req.body);
+  user.password = await hashPassword(password);
   await user.save();
-  res.send("Usuario creado correctamente");
+  res.status(201).send("Usuario creado correctamente");
 };
