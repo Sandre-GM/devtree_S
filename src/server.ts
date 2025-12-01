4755e6ab3516df4e2dea4bc37646b4a2d7670739
Mauricio Gonzalez Marmolejo <sandre9477@gmail.com>
2025-12-01 09:22:42 -0600

    I added CORS and new env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import cors from "cors";
 import express from "express";
-import 'dotenv/config';
+import "dotenv/config";
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+connectDB();
 
 const app = express();
 
-connectDB();
+app.use(cors(corsConfig));
 
 //Reading Form's Data
 app.use(express.json());
